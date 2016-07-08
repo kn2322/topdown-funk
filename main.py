bdd8c5b9c50eaccf2a6d31df2c62bf139ec0707d
Kevin Xin <dunkmast0r1337@gmail.com>
2016-07-08 15:37:13 +0800

    Working left stick and basic ability button
    After numerous issues in math and logic, the buttons are now working as
    intended, and fairly complete. This means that I can move onto creating
    the player system.

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.properties import NumericProperty, BooleanProperty
-from math import atan2, degrees, radians, inf, sin, cos, tan
+from math import atan2, degrees, radians, inf, sin, cos, tan, hypot
+from functools import partial
 
 Builder.load_string("""
-#: kivy 1.9.1   
+#: kivy 1.9.1
 
 <Game>
+    basic_attack: basic_attack
     EntityContainer:
         id: entity_container
         size: root.size
@@ -20,6 +22,8 @@ Builder.load_string("""
     LeftStick:
         center: 100, 100
     AbilityButton:
+        # Testing
+        cool_down: 6
         center: root.width - 100, 100
 
 <LeftStick>
@@ -39,7 +43,7 @@ Builder.load_string("""
         on_touch_move: root.touch_callback(args[1])
         on_touch_up: root.touch_revert()
         center: root.center
-        size: 50, 50
+        size: 40, 40
         canvas:
             Color:
                 rgba: 0, 0, 1, .5
@@ -48,18 +52,31 @@ Builder.load_string("""
                 pos: self.pos
 
 <AbilityButton>
+    cd_indicator: cd_indicator
+
     Widget:
-        canvas:
-            Ellipse:
-                size: root.size
-                pos: root.pos
-    Widget:
-        id: hitbox
-        # balance is the average of the default axis aligned bounding box and
-        # a smaller box where the corners touch the edge of the button ellipse.
-        balance: ((root.height ** 2 / 2) ** 0.5 + root.height) / 2
-        size: balance, balance
+        on_touch_down: root.touch_handler(args[1])
+        size: root.size
         center: root.center
+        canvas:
+            Color:
+                rgba: 0, 0, 1, 1
+            Ellipse:
+                size: self.size
+                pos: self.pos
+    Widget:
+        id: cd_indicator
+        size: 1, 1
+        center: root.center
+        canvas:
+            Color:
+                # First comparison is so the observer pattern doesn't go before time_step() in making the size of the circle tiny
+                # Instead of seeing the full circle from the end of the previous call
+                # size is not done using observer pattern bind
+                a: 0 if root.cool_down == root.current_cd or root.current_cd <= 0 else .3
+            Ellipse:
+                size: self.size
+                pos: self.pos
 """)
 
 Window.size = (800, 600)
@@ -68,6 +85,7 @@ Window.size = (800, 600)
 class EntityContainer(Widget):
     pass
 
+# TODO: Rewrite/fix entity system to be more extensible and follow OOP
 class Entity(Widget):
     # hp, size, ai, in order
     game_entities = {
@@ -92,9 +110,16 @@ class Player(Entity):
     def __init__(self, hp=100, size=20, **kwargs):
         super(Entity, self).__init__(hp, size, **kwargs)
 
+# deltay/deltax distance calculations
 def difference(pos1, pos2):
     return (pos2[0] - pos1[0], pos2[1] - pos1[1])
 
+# Ellipse collision workaround to default axis-aligned bounding box
+# Works by finding the distance collision point is using pythag and diference
+def in_ellipse_checker(center, touch, radius):
+    if hypot(*difference(center, touch)) <= radius:
+        return True
+    return False
 
 class LeftStick(Widget):
 
@@ -105,16 +130,13 @@ class LeftStick(Widget):
         super(LeftStick, self).__init__(**kwargs)
         self.moving_edge = self.height / 2
 
-    """For on_touch_down, it detects whether the touch is on the moving_stick,
-    before it binds the pos of the moving_stick onto the touch, this is so that
-    it doesn't bind to the edge of the leftstick when the touch moves in from
-    outside. The touching variable communicates so that touch_callback isn't
-    called unless the touch starts from the center (and on the moving_stick).
-    """
     def touch_handler(self, touch):
+        # kv doesn't init to reference moving_stick in __init__ (?)
+        self.moving_edge = self.height / 2 - self.moving_stick.height / 2
         self.touch_callback(touch)
     
     # For on_touch_move
+    # TODO: remove gradient/unecessary components
     def touch_callback(self, touch):
         if touch.x < Window.width / 2:
             delta = difference(self.center, touch.pos)
@@ -123,17 +145,18 @@ class LeftStick(Widget):
                 gradient = delta[1] / delta[0]
             except ZeroDivisionError:
                 gradient = inf
-            #print(angle, gradient)
-            if self.collide_point(*touch.pos):
+            # checks for touch collision with the control stick
+            if in_ellipse_checker(self.center, touch.pos, self.moving_edge):
                 self.moving_stick.center = touch.pos
             else:
                 # Intense right angle trigonometry
-                # Converting between rads and degrees for absolute value of degrees, don't know a better way.
-                # EDIT: Don't need abs value, it works without doing it
+                # Have trust in trig functions. :p
                 # Operates on 'triangle' where the moving_edge is the hypotnuse
                 x = self.center_x + self.moving_edge * cos(angle)
                 y = self.center_y + self.moving_edge * sin(angle)
                 self.moving_stick.center = (x, y)
+        else:
+            self.touch_revert()
 
 
     # For on_touch_up
@@ -142,22 +165,46 @@ class LeftStick(Widget):
 
 class AbilityButton(Widget):
 
-
+    cool_down = NumericProperty()
+    current_cd = NumericProperty()
+    on_cd = BooleanProperty()
 
     # Functions to be accepted with *args with tuple which has func and its cd.
     # Purpose: So that the button's behavior is able to be swapped out depending on hero.
-    def __init__(self, func=None, cool_down=None, **kwargs):
+    def __init__(self, func=partial(print, 'there is no function'), cool_down=0, **kwargs):
         super(AbilityButton, self).__init__(**kwargs)
         self.func = func
         self.cool_down = cool_down
-
+        self.current_cd = 0
+        self.on_cd = False
+        
     def assign(self, func, cool_down):
         self.func = func
         self.cool_down = cool_down
 
-    def touch_handler(self, touch):
-        pass
+    def time_step(self, dt):
+        if round(self.current_cd, 3) > 0:
+            self.current_cd -= 1/60
+            # Function to make the size of cd_indicator grow at constant rates until original size depending on cd
+            # The indicator is updated here to allow for more explicit logic instead of observer pattern via kv
+            self.cd_indicator.size = tuple(
+                -i / self.cool_down * self.current_cd + i for i in self.size
+                )
+            self.cd_indicator.center = self.center
+        else:
+            self.current_cd = 0
+            self.on_cd = False
+            self.cd.cancel()
+        print(self.cd_indicator.size)
 
+    def touch_handler(self, touch):
+        if in_ellipse_checker(self.center, touch.pos, self.height / 2) and \
+        not self.on_cd:
+            self.func()
+            self.current_cd = self.cool_down
+            # on_cd is to prevent multiple activations of the button while on cd
+            self.on_cd = True
+            self.cd = Clock.schedule_interval(self.time_step, 1/60)
 
 
 
